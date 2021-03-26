6630b98edc8caa717a287cd2d31f382f3b81f07f
Vigirl <15821988909@163.com>
2021-03-26 13:47:33 +0800

    init

---
+++ TEST002/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TEST002' 
+ * Target:  'TEST002' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
